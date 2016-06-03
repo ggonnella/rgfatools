f7acfbce213a683ef514bbe1f5cb3611e6df5e9a
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-03 11:50:21 +0200

    prepare infrastructure for method redefinition

--- lib/gfatools.rb
+++ lib/gfatools.rb
@@ -5,11 +5,22 @@ require_relative "./gfatools/traverse.rb"
 
 module GFATools
 
-  include GFATools::Edit
-  include GFATools::Traverse
-
   def gfatools_included?
     true
+  end
+
+  def self.redefine_methods(extended_methods, mod)
+    mod.class_eval do
+      extended_methods.each do |em|
+        was_private = mod.private_instance_methods.include?(em)
+        public em
+        alias_method :"#{em}_without_gfatools", em
+        alias_method em, :"#{em}_with_gfatools"
+        if was_private
+          private em, :"#{em}_without_gfatools", :"#{em}_with_gfatools"
+        end
+      end
+    end
   end
 
 end
@@ -17,5 +28,7 @@ end
 class GFA
 
   include GFATools
+  include GFATools::Edit
+  include GFATools::Traverse
 
 end
