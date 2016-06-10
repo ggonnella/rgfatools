32a19509ed93f4bd658e20ee3cf3addc9ba97c3d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-10 17:02:52 +0200

    remove debug msgs

--- bin/find_crisprs.rb
+++ bin/find_crisprs.rb
@@ -16,17 +16,7 @@ class GFA
       s.cn = (s.coverage(unit_length: @default[:unit_length],
                         count_tag: @default[:count_tag])/2).round
     end
-    if $debugmode
-      segment_names.each do |sn|
-      s = segment(sn)
-        puts "#{s.name}\t#{s.cn}\t"+
-          "#{neighbours([s.name,:B]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
-          "#{neighbours([s.name,:E]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
-          "#{links_of([s.name,:B]).size}\t"+
-          "#{links_of([s.name,:E]).size}\t"+
-          "#{s.KC}\t#{s.length}"
-      end
-    end
+    output_segment_infos if $debugmode
     segment_names.each do |sn|
       s = segment(sn)
       next if s.length < minlen or s.length > maxlen
@@ -39,46 +29,27 @@ class GFA
         circles[rt] = []
         linear[rt] = []
         links_of([sn, rt]).each do |l|
-          loop do 
+          loop do
             mv = maxvisits[sn.to_sym]
             search_circle(other_segment_end([sn,rt]),[sn,rt],l,maxvisits,0,
                           maxlen*2+s.length,[[sn,rt]],circles[rt],linear[rt])
-            #p [sn, mv]
             break if maxvisits[sn.to_sym] < 1 or maxvisits[sn.to_sym] == mv
-          end
-        end
-        if false # sn == "1000"
-          segment_names.each do |sn1|
-            s1 = segment(sn1)
-            puts "#{s1.name}\t#{s1.cn}\t"+
-              "#{neighbours([s1.name,:B]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
-              "#{neighbours([s1.name,:E]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
-              "#{links_of([s1.name,:B]).size}\t"+
-              "#{links_of([s1.name,:E]).size}\t"+
-              "#{s1.KC}\t#{s1.length}\t#{maxvisits[sn1.to_sym]}"
           end
         end
       end
       n_paths = (circles[:E].size+circles[:B].size+
                  linear[:E].size+linear[:B].size)
       if (circles[:E].size - circles[:B].size).abs > 1
-        #p [circles[:E].size,circles[:B].size]
-        #p "ncircles diff"
         next
       end
       if (linear[:E].size - linear[:B].size).abs > 0
-        #p "nlin diff"
         next
       end
       if linear[:E].size != 1
-        #p "nlin > 1"
         next
       end
       merged_circles = []
-      all_circles = circles[:E]
-      #all_circles += circles[:B].map{|c|reverse_segpath(c)}
-      #all_circles.uniq!
-      all_circles.each {|c|merged_circles << merge_crisprs_path(c,s,:E)}
+      circles[:E].each {|c|merged_circles << merge_crisprs_path(c,s,:E)}
       before = merge_crisprs_path(linear[:B][0],s,:B)
       after = merge_crisprs_path(linear[:E][0],s,:E)
       next if merged_circles.size < minrepeats
@@ -130,6 +101,18 @@ class GFA
 
   private
 
+  def output_segment_infos
+    segment_names.each do |sn|
+      s = segment(sn)
+      puts "#{s.name}\t#{s.cn}\t"+
+        "#{neighbours([s.name,:B]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
+        "#{neighbours([s.name,:E]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
+        "#{links_of([s.name,:B]).size}\t"+
+        "#{links_of([s.name,:E]).size}\t"+
+        "#{s.KC}\t#{s.length}"
+    end
+  end
+
   def merge_crisprs_path(segpath,repeat,repeat_end)
     merged = create_merged_segment(segpath, merged_name: :short,
                                  disable_tracking: true)[0]
@@ -141,24 +124,11 @@ class GFA
 
   def search_circle(goal,from,l,maxvisits,dist,maxdist,path,circles,linear)
     dest_end = l.other_end(from)
-    debug = false # goal[0] == "1000"
-    if debug
-      puts
-      puts "Visiting: #{dest_end[0]}"
-      puts "- path: #{path.map{|x|x[0]}.inspect}"
-      puts "- circles size: #{circles.size}"
-      puts "- linear size: #{linear.size}"
-      puts
-    end
     dest = segment(dest_end[0])
     destsym = dest.name.to_sym
     maxvisits[destsym] ||= dest.cn
-    if debug and from[0] == "999"
-      puts "Visiting a nb of 999 (mv=#{maxvisits[:"999"]}) #{dest_end[0]} / maxvisits=#{maxvisits[destsym]}"
-    end
     se = other_segment_end(dest_end)
     if dest_end == goal
-      puts "goal reached" if debug
       new_path = path.dup
       new_path << se
       new_path[0..-2].each {|x| maxvisits[x[0].to_sym] -= 1}
@@ -166,18 +136,15 @@ class GFA
       return
     end
     if maxvisits[destsym] == 0
-      puts "too many visits" if debug
       return
     end
     if path.any?{|x|x[0]==dest_end[0]}
-      puts "path circular before goal" if debug
       return
     end
     new_path = path.dup
     new_path << se
     dist += dest.length - l.overlap[0][0]
     if dist > maxdist
-      puts "path linear as too long" if debug
       new_path = path.dup
       new_path << se
       new_path[0..-1].each {|x| maxvisits[x[0].to_sym] -= 1}
@@ -186,7 +153,6 @@ class GFA
     end
     ls = links_of(se)
     if ls.size == 0
-      puts "path linear as end reached" if debug
       new_path[0..-1].each {|x| maxvisits[x[0].to_sym] -= 1}
       linear << new_path
       return
