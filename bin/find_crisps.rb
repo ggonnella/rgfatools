b23bc73fd3e3dd376573995492f565faf70cbc07
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-10 02:19:48 +0200

    use correct coverages

--- bin/find_crisps.rb
+++ bin/find_crisps.rb
@@ -8,12 +8,12 @@ $debugmode = false
 
 class GFA
 
-  def find_crisps(minrepeats=3,minlen=24,maxlen=50,k=headers_data[:ks])
-    raise if k.nil?
+  def find_crisps(minrepeats=3,minlen=24,maxlen=50)
     ls = {}
     segment_names.each do |sn|
       s = segment(sn)
-      s.cn = approx_cn(k,s.KC,s.length).round
+      s.cn = s.coverage(unit_length: @default[:unit_length],
+                        count_tag: @default[:count_tag])
     end
     if $debugmode
       segment_names.each do |sn|
@@ -23,8 +23,7 @@ class GFA
           "#{neighbours([s.name,:E]).map{|nb|segment(nb[0]).cn}.inject(:+)}\t"+
           "#{links_of([s.name,:B]).size}\t"+
           "#{links_of([s.name,:E]).size}\t"+
-          "#{s.kc}\t#{s.length}\t"+
-          "#{approx_cn(k,s.KC,s.length).round}"
+          "#{s.KC}\t#{s.length}"
       end
     end
     segment_names.each do |sn|
@@ -44,20 +43,22 @@ class GFA
       end
       n_paths = (circles[:E].size+circles[:B].size+
                  linear[:E].size+linear[:B].size)
-      diff = (s.cn * 2 - n_paths)/2
-      next if (circles[:E].size - circles[:B].size).abs > diff
+      next if (circles[:E].size - circles[:B].size).abs > 0
       next if (linear[:E].size - linear[:B].size).abs > 0
       next if linear[:E].size != 1
       merged_circles = []
-      all_circles = (circles[:B].map{|c|reverse_segpath(c)} + circles[:E]).uniq
+      all_circles = circles[:E]
+      #all_circles += circles[:B].map{|c|reverse_segpath(c)}
+      #all_circles.uniq!
       all_circles.each {|c|merged_circles << merge_crisps_path(c,s,:E)}
       before = merge_crisps_path(linear[:B][0],s,:B)
       after = merge_crisps_path(linear[:E][0],s,:E)
       next if merged_circles.size < minrepeats
       instances = 1
+      possible_instances = 0
       merged_circles.each do |seq|
-        if seq.length > s.length * 2
-          instances += 2
+        if seq.length > s.length + minlen
+          possible_instances += 1
         else
           instances += 1
         end
@@ -66,15 +67,18 @@ class GFA
       puts
       puts "  Before: sequence = ...#{before[-50..-1]}"
       puts
-      puts "  Repeat: instances =~ #{instances}; "+
+      if possible_instances > 0
+        instances = "#{instances}..#{instances+possible_instances}"
+      end
+      puts "  Repeat: instances = #{instances}; "+
            "length = #{s.length};\t"+
            "sequence = #{s.sequence}"
       puts
       puts "  Spacers:"
       asterisk = false
       merged_circles.each_with_index do |seq, i|
-        if seq.length > s.length * 2
-          str = "*"
+        if seq.length > s.length + minlen
+          str = "=#{s.length}+2*#{(seq.length.to_f - s.length)/2}"
           asterisk = true
         else
           str = ""
@@ -95,13 +99,10 @@ class GFA
   def merge_crisps_path(segpath,repeat,repeat_end)
     merged = create_merged_segment(segpath, merged_name: :short,
                                  disable_tracking: true)[0]
-    sequence = merged.sequence[repeat.sequence.length..-(1+repeat.sequence.length)]
+    sequence = merged.sequence[repeat.
+                                 sequence.length..-(1+repeat.sequence.length)]
     sequence = sequence.rc if repeat_end == :B
     return sequence
-  end
-
-  def approx_cn(k,kc,l)
-    (kc+l-k+1).to_f/(l-k+3)
   end
 
   def search_circle(goal,from,l,maxvisits,dist,maxdist,path,circles,linear)
@@ -142,5 +143,7 @@ if (ARGV.size == 0)
   exit 1
 end
 gfa = GFA.from_file(ARGV[0])
+gfa.set_default_count_tag(:KC)
+gfa.set_count_unit_length(gfa.headers_data[:ks])
 gfa.find_crisps
 
