f6b3e1844a169cb82d4184b4fb9dfec2e33f9607
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-08-04 19:46:37 +0200

    split edit test in copy_number and linear_paths

--- test/test_gfatools_copy_number.rb
+++ test/test_gfatools_copy_number.rb
@@ -1,9 +1,9 @@
 require_relative "../lib/rgfatools.rb"
 require "test/unit"
 
-class TestRGFAToolsEdit < Test::Unit::TestCase
+class TestRGFAToolsCopyNumber < Test::Unit::TestCase
 
-  def test_delete_low_coverate_segments
+  def test_delete_low_coverage_segments
     gfa = ["S\t0\t*\tRC:i:600\tLN:i:100",
            "S\t1\t*\tRC:i:6000\tLN:i:100",
            "S\t2\t*\tRC:i:60000\tLN:i:100"].to_rgfa
@@ -39,43 +39,6 @@ class TestRGFAToolsEdit < Test::Unit::TestCase
     assert_equal([:"1",:"2",:"3",:"2b",:"3b",:"3c"], gfa.segment_names)
     gfa.compute_copy_numbers(9)
     assert(gfa.segments.map(&:cn).all?{|cn|cn == 1})
-  end
-
-  def test_linear_path_merging
-    s = ["S\t0\tACGA",
-         "S\t1\tACGA",
-         "S\t2\tACGA",
-         "S\t3\tACGA"]
-    l = ["L\t0\t+\t1\t+\t1M",
-         "L\t1\t+\t2\t-\t1M",
-         "L\t2\t-\t3\t+\t1M"]
-    gfa = RGFA.new
-    (s + l).each {|line| gfa << line }
-    gfa.merge_linear_path([["0", :E],["1", :E],["2", :B],["3", :E]])
-    assert_nothing_raised {gfa.segment!("0_1_2^_3")}
-    assert_equal("ACGACGACGTCGA", gfa.segment("0_1_2^_3").sequence)
-  end
-
-  def test_linear_path_merge_all
-    s = ["S\t0\t*",
-         "S\t1\t*",
-         "S\t2\t*",
-         "S\t3\t*"]
-    l = ["L\t0\t+\t1\t+\t1M",
-         "L\t1\t+\t2\t-\t1M",
-         "L\t2\t-\t3\t+\t1M"]
-    gfa = RGFA.new
-    (s + l).each {|line| gfa << line }
-    gfa.merge_linear_paths
-    assert_equal([:"0_1_2^_3"], gfa.segment_names)
-    l = ["L\t0\t+\t1\t+\t1M",
-         "L\t0\t+\t2\t+\t1M",
-         "L\t1\t+\t2\t-\t1M",
-         "L\t2\t-\t3\t+\t1M"].map(&:to_rgfa_line)
-    gfa = RGFA.new
-    (s + l).each {|line| gfa << line }
-    gfa.merge_linear_paths
-    assert_equal([:"0",:"3",:"1_2^"], gfa.segments.map(&:name))
   end
 
 end
