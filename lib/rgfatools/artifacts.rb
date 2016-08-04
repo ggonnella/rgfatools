5497bf41311f28e3ccf1c5f8a18df55f1b36e61a
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-08-04 19:42:51 +0200

    test artifacts module

--- lib/rgfatools/artifacts.rb
+++ lib/rgfatools/artifacts.rb
@@ -9,18 +9,20 @@ module RGFATools::Artifacts
   # @return [RGFA] self
   def remove_small_components(minlen)
     rm(connected_components.select {|cc|
-      cc.map{|sn|segment!(sn).length}.reduce(:+) < minlen })
+      cc.map{|sn|segment(sn).length}.reduce(:+) < minlen })
     self
   end
 
-  # Remove dead end segments, whose sequence length is under a specified value.
+  # Remove end segments, whose sequence length is under a specified value.
   # @param minlen [Integer] the minimum length
   # @return [RGFA] self
   def remove_dead_ends(minlen)
-    rm(segments.select {|s|
-      c = connectivity(s); s.length < minlen and
-        c[0] == 0 or c[1] == 0 and
-        !cut_segment?(s) })
+    segments.each do |s|
+      c = connectivity(s)
+      rm(s) if s.length < minlen and
+        (c[0] == 0 or c[1] == 0) and
+          !cut_segment?(s)
+    end
     self
   end
 
